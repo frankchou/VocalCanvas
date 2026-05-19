9023dac3401e94ec25152e75c4fcf7cc199399d2
frankchou <44085585+frankchou@users.noreply.github.com>
2026-05-19 12:37:09 +0000

    add apple signup to signup page

--- src/app/(marketing)/signup/page.tsx
+++ src/app/(marketing)/signup/page.tsx
@@ -151,6 +151,12 @@ export default function SignupPage(): React.JSX.Element {
               </svg>
               {isZh ? '使用 LINE 註冊' : 'Sign up with LINE'}
             </button>
+            <button className="oauth-btn" type="button">
+              <svg width={20} height={20} viewBox="0 0 24 24" fill="#000">
+                <path d="M17.05 20.28c-.98.95-2.05.8-3.08.35-1.09-.46-2.09-.48-3.24 0-1.44.62-2.2.44-3.06-.35C2.79 15.25 3.51 7.59 9.05 7.31c1.35.07 2.29.74 3.08.8 1.18-.24 2.31-.93 3.57-.84 1.51.12 2.65.72 3.4 1.8-3.12 1.87-2.38 5.98.48 7.13-.57 1.5-1.31 2.99-2.54 4.09zM12 7.25c-.15-2.23 1.66-4.07 3.74-4.25.29 2.58-2.34 4.5-3.74 4.25z" />
+              </svg>
+              <span>{isZh ? '使用 Apple 註冊' : 'Sign up with Apple'}</span>
+            </button>
           </div>
 
           <div className="divider">
